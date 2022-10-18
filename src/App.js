e374c4172ed46d68f9209495ef4a3b4597b97461
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-10-18 18:58:16 +0200

    Add data layer to app component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import GuessLetters from "./components/GuessLetters/GuessLetters";
 import Hangman from "./components/Hangman/Hangman";
 import Letters from "./components/Letters/Letters";
@@ -5,6 +6,18 @@ import Result from "./components/Result/Result";
 import UsedLetters from "./components/UsedLetters/UsedLetters";
 
 function App() {
+  const [usedLetters, setUsedLetters] = useState(new Set());
+  const [totalErrors, setTotalErrors] = useState(0);
+
+  const randomWords = [
+    "arbol",
+    "camisa",
+    "pantalla",
+    "rendimiento",
+    "ahorcado",
+  ];
+  const alphabetLetters = "abcdefghijklmnñopqrstuvwxyz";
+
   return (
     <>
       <div className="container">
